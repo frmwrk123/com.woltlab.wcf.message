e8e56b62f7b80c034d08698c5da258eb044648ee
Tim Düsterhus <timwolla@arcor.de>
2012-07-03 21:16:14 +0200

    Adding first version of AbstractFeedPage

---
+++ files/lib/page/AbstractFeedPage.class.php
@@ -0,0 +1,46 @@
+<?php
+namespace wcf\page;
+use wcf\system\exception\IllegalLinkException;
+use wcf\util\ArrayUtil;
+
+/**
+ * Generates RSS 2-Feeds.
+ * 
+ * @author	Tim Düsterhus
+ * @copyright	2001-2012 WoltLab GmbH
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package	com.woltlab.wcf.message
+ * @subpackage	page
+ * @category 	Community Framework
+ */
+abstract class AbstractFeedPage extends AbstractAuthedPage {
+	public $templateName = 'rssFeed';
+	public $objectIDs = array();
+	
+	/**
+	 * @see wcf\page\IPage::readParameters()
+	 */
+	public function readParameters() {
+		parent::readParameters();
+		
+		if (isset($_REQUEST['id'])) {
+			if (is_array($_REQUEST['id'])) {
+				// ?id[]=1337&id[]=9001
+				$this->objectIDs = ArrayUtil::toIntegerArray($_REQUEST['id']);
+			}
+			else {
+				// ?id=1337 or ?id=1337,9001
+				$this->objectIDs = ArrayUtil::toIntegerArray(explode(',', $_REQUEST['id']));
+			}
+		}
+	}
+	
+	/**
+	 * @see wcf\page\IPage::show()
+	 */
+	public function show() {
+		@header('Content-Type: application/rss+xml');
+		
+		parent::show();
+	}
+}
